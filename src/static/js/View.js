e4cc79588c226ebbccee9ad2860b0b880547c362
AkselCaubel <aksel.paulet@gmail.com>
2025-01-22 09:50:05 +0000

    add img in game

--- src/static/js/View.js
+++ src/static/js/View.js
@@ -1,9 +1,28 @@
 class View {
     constructor() {
+        
         this._canvas = document.getElementById('my_canvas');
+        this._canvas.style.backgroundImage = 'url(../img/bck@2x.png)';
+        this._canvas.style.backgroundSize = 'cover';
         this.ctx     = this._canvas.getContext('2d');
+        
         this._hold_right = false;
         this._hold_left = false;
+        
+        /* Chargement des images */
+        this.HEXTILES_IMAGE = new Image();
+        this.HEXTILES_IMAGE.src = '../img/game-tiles.png';
+        this.DOODLE_LEFT = new Image();
+        this.DOODLE_LEFT.src = '../img/lik-left@2x.png';
+        this.DOODLE_RIGHT = new Image();
+        this.DOODLE_RIGHT.src = '../img/lik-right@2x.png';
+
+        this.doodle = this.DOODLE_RIGHT;
+
+        /* Les coordonnées des images dans le fichier game-tiles.png */
+        this.green_slab = {x: 1, y: 1, w: 57, h: 15};
+        this.blue_slab = {x: 1, y: 19, w: 57, h: 15};
+        this.white_slab = {x: 1, y: 55, w: 57, h: 15};
 
         this.Events();
     }
@@ -19,10 +38,12 @@ class View {
                     case 'ArrowLeft': // Move left.
                         this._hold_left = true;
                         this.b_SetDirection(-1);
+                        this.doodle = this.DOODLE_LEFT;
                         break;
                     case 'ArrowRight': // Move right.
                         this._hold_right = true;
                         this.b_SetDirection(1);
+                        this.doodle = this.DOODLE_RIGHT;
                         break;
                 }
             }
@@ -49,20 +70,21 @@ class View {
     Display(position) {
         let x = position.x;
         let y = position.y;
+        console.log(position)
         
         this.ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
-
-        // Dessiner les lignes.
-        this.ctx.beginPath(); // Nouveau tracé.
-        this.ctx.moveTo(100, 0); // Déplacement du crayon en (100, 0).
-        this.ctx.lineTo(100, 100); // Dessiner la ligne en (150, 100).
-        this.ctx.stroke(); // Afficher la ligne.
-        this.ctx.beginPath(); // Nouveau tracé.
-        this.ctx.moveTo(0, 100); // Déplacement du crayon en (100, 0).
-        this.ctx.lineTo(100, 100); // Dessiner la ligne en (150, 100).
-        this.ctx.stroke(); // Afficher la ligne.
         
-        // Dessiner un rectangle plein.
-        this.ctx.fillRect(x, y, 10, 10);
+        // Dessiner les lignes.
+        // this.ctx.beginPath(); // Nouveau tracé.
+        // this.ctx.moveTo(100, 0); // Déplacement du crayon en (100, 0).
+        // this.ctx.lineTo(100, 100); // Dessiner la ligne en (150, 100).
+        // this.ctx.stroke(); // Afficher la ligne.
+        // this.ctx.beginPath(); // Nouveau tracé.
+        // this.ctx.moveTo(0, 100); // Déplacement du crayon en (100, 0).
+        // this.ctx.lineTo(100, 100); // Dessiner la ligne en (150, 100).
+        // this.ctx.stroke(); // Afficher la ligne.
+        
+        // Dessiner doodle.
+        this.ctx.drawImage(this.doodle,0,0,140,120,x, y, 140/2.5, 120/2.5);
     }
 }
