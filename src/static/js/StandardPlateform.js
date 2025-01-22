e0a354b205fa75e89ae3ec628b1ac9bae1294597
Corentin Claudel <corentin@cclaudel.fr>
2025-01-22 19:13:34 +0100

    Ajout de l'attribut valeur à la documentation

--- src/static/js/StandardPlateform.js
+++ src/static/js/StandardPlateform.js
@@ -4,6 +4,7 @@ class StandardPlateform {
 
     /**
      * Crée une plateform standard qui ne bougent pas, ne disparais pas et ne se brise pas
+     * Met la valeur Value à 0
      * @param {int} xCord Correpond à la coordonnées x de la grille
      * @param {int} yCord Correpond à la coordonénes y de la grille
      */
