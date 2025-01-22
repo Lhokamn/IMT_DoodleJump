dcca1e05513c949279d825d2729b3caf85454323
Corentin Claudel <corentin@cclaudel.fr>
2025-01-22 19:23:30 +0100

    Correction du nom de la fonction

--- src/static/js/Plateform.js
+++ src/static/js/Plateform.js
@@ -1,12 +1,35 @@
-class plateform{
+class Plateform{
 
     /**
-     * Perrmet de créer une nouvelle plateform
+     * Permet de créer une nouvelle plateform
+     * @param {int} value Correspond au type de platforme voulu. Il est renseigné par les classe fille automatiquement
+     * @param {int} xCord Correpond à la coordonnées x de la grille
+     * @param {int} yCord Correpond à la coordonénes y de la grille
      */
-    constructor(){
-        this.type
-        this.xCord
-        this.yCord
-        this.state
+    constructor(value, xCord, yCord){
+
+
+        if (this.constructor == Plateform){
+            throw new TypeError('Abstract class "Plateform" cannot be instantiated directly');
+        }
+        this.value = value;
+        this.xCord = xCord;
+        this.yCord = yCord;
+    }
+
+    /**
+     * Récupère la valeur de la plateforme
+     * @returns this.value correspond à la valeur de la plateform
+     */
+    GetValue(){
+        return this.value;
+    }
+
+    /**
+     * Met à jour la valeur de la plateform
+     * @param {*} newValue 
+     */
+    setValue(newValue){
+        this.value = newValue;
     }
 }
